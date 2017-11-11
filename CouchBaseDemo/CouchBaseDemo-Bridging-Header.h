de560d90dfed62452f4c12459deb9f0f96b33a5d
Shobhakar Tiwari <shobhakartiwari@shobhakars-air.lan>
2017-11-11 21:34:58 +0530

    Initial Commit

---
+++ CouchBaseDemo/CouchBaseDemo-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  CouchBaseDemo-Bridging-Header.h
+//  CouchBaseDemo
+//
+//  Created by Shobhakar Tiwari on 10/11/17.
+//  Copyright © 2017 Shobhakar. All rights reserved.
+//
+
+#ifndef CouchBaseDemo_Bridging_Header_h
+#define CouchBaseDemo_Bridging_Header_h
+
+#endif /* CouchBaseDemo_Bridging_Header_h */
+#import "CouchbaseLite/CouchbaseLite.h"
+
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'CouchBaseDemo' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for CouchBaseDemo
+pod 'couchbase-lite-ios', '~> 1.4.0'
+
+end
