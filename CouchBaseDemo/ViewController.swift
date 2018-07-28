ae59245167c7f24b4ca00c60562ba6b1b2245163
shubh <shubh_tiwari@hotmail.com>
2018-07-29 00:42:49 +0530

    Added by shubh_tiwari: test push

--- CouchBaseDemo/ViewController.swift
+++ CouchBaseDemo/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //Edit by latest shubh_tiwari
     override func viewDidLoad() {
         super.viewDidLoad()
         // Create a manager
